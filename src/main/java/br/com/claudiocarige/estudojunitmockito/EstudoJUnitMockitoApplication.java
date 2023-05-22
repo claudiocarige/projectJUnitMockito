4c6f1c9f50e8ad760a2cf9a40daa7004873ed042
Claudio Carige <ccarige@gmail.com>
2023-05-22 20:46:15 -0300

    first commit - created project

---
+++ src/main/java/br/com/claudiocarige/estudojunitmockito/EstudoJUnitMockitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.claudiocarige.estudojunitmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoJUnitMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudoJUnitMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/claudiocarige/estudojunitmockito/EstudoJUnitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.claudiocarige.estudojunitmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoJUnitMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
