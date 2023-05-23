94291d0154ea6da052b8f4be1d4b3f05cb9cf8b8
Claudio Carige <ccarige@gmail.com>
2023-05-22 21:13:25 -0300

    Created Interface UserService

--- src/main/java/br/com/claudiocarige/estudojunitmockito/service/UserService.java
+++ src/main/java/br/com/claudiocarige/estudojunitmockito/service/UserService.java
@@ -1,2 +1,12 @@
-package br.com.claudiocarige.estudojunitmockito.service;public class UserService {
+package br.com.claudiocarige.estudojunitmockito.service;
+
+import br.com.claudiocarige.estudojunitmockito.domain.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    User findById(Integer id);
+
+    List<User> findAll();
 }
